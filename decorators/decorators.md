7b1a730ebac02eddd03692ada78b68b34ac4f6fc
Parminder Thindal <parminderthindal@pst-mac-studio.local>
2023-12-10 15:00:24 +0000

    Feat - initial decorator files

---
+++ decorators/decorators.md
@@ -0,0 +1,6 @@
+### Decorators
+
+- a callable that takes another function an an argument (the decorated function)
+
+A decorator may peform some processing with the decorated function, and returns or may replace it
+ with another function or callable object.
---
+++ functions/function_objects.py
@@ -0,0 +1,29 @@
+def factorial(n):
+    """returns n!"""
+    
+    return 1 if n < 2 else n * factorial(n-1)
+
+
+factorial(42)
+
+factorial.__doc__
+
+type(factorial)
+
+help(factorial)
+
+
+
+fact = factorial
+
+fact(5)
+
+map(fact, range(10))
+map(factorial, range(10))
+
+list(map(fact, range(10)))
+list(map(factorial, range(10)))
+
+map.__doc__
+
+help(map)
