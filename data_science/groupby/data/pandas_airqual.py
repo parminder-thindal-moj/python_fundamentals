e4572c31cc25dbcdec033e2087137b24d040c39c
Parminder S Thindal <parminderthindal@gmail.com>
2024-02-23 20:36:24 +0000

    Temp save - work in progress

---
+++ data_science/groupby/data/pandas_airqual.py
@@ -0,0 +1,22 @@
+# pandas_airqual.py
+
+import pandas as pd
+
+df = (
+    pd.read_csv(
+        "groupby-data/airqual.csv",
+        parse_dates=[["Date", "Time"]],
+        na_values=[-200],
+        usecols=["Date", "Time", "CO(GT)", "T", "RH", "AH"],
+    )
+    .rename(
+        columns={
+            "CO(GT)": "co",
+            "Date_Time": "tstamp",
+            "T": "temp_c",
+            "RH": "rel_hum",
+            "AH": "abs_hum",
+        }
+    )
+    .set_index("tstamp")
+)
